a5d5a668b457c8b8699511963f5fa4eac1bfb03b
Vlad Egipti <49652419+vlad-egipti@users.noreply.github.com>
2019-05-23 21:40:25 +0400

    Password reset provided

--- customauth/urls.py
+++ customauth/urls.py
@@ -8,4 +8,8 @@ urlpatterns = [
     path('signup/', views.SignUp.as_view(), name='signup'),
     path('password-change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
     path('password-change/done', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
