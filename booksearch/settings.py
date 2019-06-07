7c57196f8c53938b574d5933efa4df0ae2602ca2
Vlad Egipti <49652419+vlad-egipti@users.noreply.github.com>
2019-06-07 18:14:00 +0400

    Add header and navbar

--- booksearch/settings.py
+++ booksearch/settings.py
@@ -126,6 +126,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Authentication model definition
 AUTH_USER_MODEL = 'customauth.CustomUser'
